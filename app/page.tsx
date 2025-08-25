ecd0ebe98ee7253ea104b1256120bee61fa06164
Samuel <camka14@gmail.com>
2025-08-25 14:10:45 -0700

    Fix images for app links

--- app/page.tsx
+++ app/page.tsx
@@ -125,7 +125,7 @@ export default function PersonalWebsite() {
                   className="inline-block"
                 >
                   <Image
-                    src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                    src="images/buttons/googlePlay.svg"
                     alt="Get it on Google Play"
                     width={200}
                     height={64}
@@ -140,7 +140,7 @@ export default function PersonalWebsite() {
                   className="inline-block"
                 >
                   <Image
-                    src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                    src="images/buttons/appStore.svg"
                     alt="Download on the App Store"
                     width={200}
                     height={64}
